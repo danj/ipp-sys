ac4c2fba84fbbc0775a4e4177dc20a2b68389df2
danj <dan@beamr.com>
2019-09-20 14:46:26 +0300

    fix dependencies

--- link-ippcv/src/lib.rs
+++ link-ippcv/src/lib.rs
@@ -1,3 +1,5 @@
 #![allow(unused_extern_crates)]
 extern crate link_ippcore;
+extern crate link_ippvm;
 extern crate link_ipps;
+extern crate link_ippi;
--- link-ippdc/src/lib.rs
+++ link-ippdc/src/lib.rs
@@ -1,3 +1,4 @@
 #![allow(unused_extern_crates)]
 extern crate link_ippcore;
 extern crate link_ipps;
+extern crate link_ippvm;
